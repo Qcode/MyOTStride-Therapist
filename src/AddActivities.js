1c0d2959b55d043cede3ab42675ada82fcb17c08
Kiran Edwardson <ryval.edwardson@gmail.com>
2018-09-06 00:10:58 -0400

    Use Formik Form and Field in AddActivities

--- src/AddActivities.js
+++ src/AddActivities.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import PropTypes from 'prop-types';
 
-function AddGoals(props) {
+function AddActivities(props) {
   return (
     <div className="div--addItems">
       <h2>Add Activities Here</h2>
@@ -14,70 +14,44 @@ function AddGoals(props) {
           startDate: '',
         }}
         onSubmit={props.addFunction}
-        render={({
-          values,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit}>
+        render={({ values, handleSubmit, isSubmitting }) => (
+          <Form onSubmit={handleSubmit}>
             <br />
             <label htmlFor="title">
               Title:
-              <input
-                id="title"
-                name="title"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.title}
-              />
+              <Field id="title" name="title" value={values.title} />
             </label>
             <br />
             <label htmlFor="description">
               description:
-              <input
+              <Field
                 id="description"
                 name="description"
-                onChange={handleChange}
-                onBlur={handleBlur}
                 value={values.description}
               />
             </label>
             <br />
             <label htmlFor="startDate">
               Start Date:
-              <input
-                id="startDate"
-                type="date"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.startDate}
-              />
+              <Field id="startDate" type="date" value={values.startDate} />
             </label>
             <br />
             <label htmlFor="endDate">
               End Date:
-              <input
-                id="endDate"
-                type="date"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.endDate}
-              />
+              <Field id="endDate" type="date" value={values.endDate} />
             </label>
             <br />
             <button type="submit" disabled={isSubmitting}>
               Submit
             </button>
-          </form>
+          </Form>
         )}
       />
     </div>
   );
 }
-AddGoals.propTypes = {
+AddActivities.propTypes = {
   addFunction: PropTypes.func.isRequired,
 };
 
-export default AddGoals;
+export default AddActivities;
