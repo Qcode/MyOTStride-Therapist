28c13bd86fd94080d3f221aaadf748bcf01aeb18
Ross Evans <therealrossevans@gmail.com>
2018-09-08 09:58:36 -0400

    Refactor login form

--- src/Login.js
+++ src/Login.js
@@ -1,77 +1,57 @@
 import React from 'react';
-import { Formik, Field, Form } from 'formik';
+import { Formik, withFormik, Field, Form } from 'formik';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Api from './Api';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-    };
-    this.login = this.login.bind(this);
-  }
-
-  login(values, actions) {
-    Api.request('login', {
-      method: 'POST',
-      body: {
-        email: values.email,
-        password: values.password,
-        therapist: true,
-      },
-    })
-      .then(userData => {
-        Api.setToken(userData.token);
-        Api.setTherapistId(userData.id);
-        this.props.history.push('/patients');
-      })
-      .catch(err => {
-        this.setState({ error: err });
-        actions.setSubmitting(false);
-      });
-  }
-
-  render() {
-    return (
-      <div id="LoginInfo">
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          onSubmit={this.login}
-          render={({ values, isSubmitting }) => (
-            <Form>
-              <Field
-                type="email"
-                name="email"
-                placeholder="email"
-                value={values.email}
-              />
-              <br />
-              <Field
-                placeholder="password"
-                type="password"
-                name="password"
-                value={values.password}
-              />
-              <br />
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </Form>
-          )}
-        />
-        {this.state.error && 'Error logging in.'}
-      </div>
-    );
-  }
+function Login(props) {
+  return (
+    <Form>
+      <Field
+        type="email"
+        name="email"
+        placeholder="email"
+        value={props.values.email}
+      />
+      <br />
+      <Field
+        placeholder="password"
+        type="password"
+        name="password"
+        value={props.values.password}
+      />
+      <br />
+      <button type="submit" disabled={props.isSubmitting}>
+        Login
+      </button>
+      {props.errors.failedSubmit && <p>{props.errors.failedSubmit}</p>}
+    </Form>
+  );
 }
 
 Login.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func }).isRequired,
 };
 
-export default withRouter(Login);
+export default withRouter(
+  withFormik({
+    handleSubmit: (values, formikBag) =>
+      Api.request('login', {
+        method: 'POST',
+        body: {
+          email: values.email,
+          password: values.password,
+          therapist: true,
+        },
+      })
+        .then(userData => {
+          Api.setToken(userData.token);
+          Api.setTherapistId(userData.id);
+          formikBag.props.history.push('/patients');
+        })
+        .catch(err => {
+          formikBag.setSubmitting(false);
+          formikBag.setErrors({ failedSubmit: 'Error logging in.' });
+        }),
+  })(Login),
+);
