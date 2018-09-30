177e5c9989352a7eb4cda9822fa843efe924e79c
Kiran Edwardson <ryval.edwardson@gmail.com>
2018-09-29 21:15:53 -0400

    add TextField UI adjustments and login page styling

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -1,29 +1,43 @@
 import React from 'react';
-import { withFormik, Field, Form } from 'formik';
+import { withFormik, Form } from 'formik';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
 import Api from '../Api';
+import './Login.css';
 
 function Login(props) {
   return (
     <div>
-      <h1>Login</h1>
-      <Form>
-        <Field
+      <h1 className="Login__Title">Login</h1>
+      <Form className="Login__Form">
+        <TextField
+          label="Email"
+          className="Login__Input"
+          placeholder="Email"
           type="email"
           name="email"
-          placeholder="email"
+          onChange={props.handleChange}
           value={props.values.email}
+          variant="outlined"
         />
         <br />
-        <Field
-          placeholder="password"
+        <TextField
+          label="Password"
+          className="Login__Input"
+          placeholder="Password"
           type="password"
           name="password"
+          onChange={props.handleChange}
           value={props.values.password}
+          variant="outlined"
         />
         <br />
-        <button type="submit" disabled={props.isSubmitting}>
+        <button
+          className="Login__Button"
+          type="submit"
+          disabled={props.isSubmitting}
+        >
           Login
         </button>
         {props.errors.failedSubmit && <p>{props.errors.failedSubmit}</p>}
@@ -38,6 +52,7 @@ Login.propTypes = {
     password: PropTypes.string,
   }).isRequired,
   isSubmitting: PropTypes.bool.isRequired,
+  handleChange: PropTypes.func.isRequired,
   errors: PropTypes.shape({
     failedSubmit: PropTypes.string,
   }).isRequired,
