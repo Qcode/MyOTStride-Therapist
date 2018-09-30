a1d1c6092f7490ded5173e2ce02f1173cfc16694
Kiran Edwardson <ryval.edwardson@gmail.com>
2018-09-30 09:43:39 -0400

    add color palette theme, MUI button and highlighting when selecting login fields

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -3,6 +3,7 @@ import { withFormik, Form } from 'formik';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import Api from '../Api';
 import './Login.css';
 
@@ -14,32 +15,41 @@ function Login(props) {
         <TextField
           label="Email"
           className="Login__Input"
-          placeholder="Email"
           type="email"
           name="email"
           onChange={props.handleChange}
           value={props.values.email}
           variant="outlined"
+          margin="normal"
+          InputLabelProps={{ shrink: true }}
+          placeholder="therapist@therapy.org"
+          required
         />
         <br />
         <TextField
           label="Password"
           className="Login__Input"
-          placeholder="Password"
           type="password"
           name="password"
           onChange={props.handleChange}
           value={props.values.password}
           variant="outlined"
+          margin="normal"
+          InputLabelProps={{ shrink: true }}
+          placeholder="********"
+          color="secondary"
+          required
         />
         <br />
-        <button
-          className="Login__Button"
+        <Button
+          variant="contained"
           type="submit"
+          className="Login__Button"
           disabled={props.isSubmitting}
+          color="primary"
         >
           Login
-        </button>
+        </Button>
         {props.errors.failedSubmit && <p>{props.errors.failedSubmit}</p>}
       </Form>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,39 @@ import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './Containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      main:"#00a388",
+    },
+    secondary:{
+      main:"#ff8552",
+    },
+  },
+  overrides: {
+    MuiNotchedOutline: {
+      focused: {
+        "border-color": "#ff8552"
+      }
+    },
+    MuiFormLabel: {
+      root: {
+        "&$focused": {
+          color: "#ff8552"
+        }
+      }
+    }
+  }
+})
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <App />
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 registerServiceWorker();
